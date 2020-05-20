b4f179e4261bedd911b2b30ca02747953048eb8b
sulthan1625 <patansultan786@gmail.com>
2020-05-20 14:04:22 +0530

    first commit

---
+++ machine-learning-ex1/ex1/Untitled6.m
@@ -0,0 +1,7 @@
+data = load('ex1data1.txt'); % read comma separated data
+X = data(:, 1);
+y = data(:, 2); 
+m = length(y); 
+plot(X, y, 'rx', 'MarkerSize', 10); % Plot the data 
+ylabel('Profit in $10,000s'); % Set the y?axis label 
+xlabel('Population of City in 10,000s'); 
